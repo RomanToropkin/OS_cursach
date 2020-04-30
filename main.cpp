51b29f8805ffc1c51c7c660479a13cf7ea1c48ba
roman <roman@roman-pc>
2020-04-30 13:38:22 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: roman
+ *
+ * Created on 30 апреля 2020 г., 13:24
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
